fcaa73ec233d69e41669de4967845f5f72ae6420
WhWhWhWh1243 <55350554+WhWhWhWh1243@users.noreply.github.com>
2019-09-22 16:32:11 +0300

    Add files via upload

---
+++ rrbvfvfv/fv.py
@@ -0,0 +1,48 @@
+'''
+s = 'hello world'
+#crezы
+#Index  - порядковый номер элемента, начинается с
+print(s[3])#вывод елемента по индексу
+print(s[3:5])#Добавление выреза
+print(s[3:6:2]) #дабавление шага выборки
+print(s[::-1])
+#операции со строками
+print('Hello' + 'World')
+print ('YAYAYAYAYAYAYA' * 3)
+
+#Методы
+print( 'Hello World'.split('hello') ) #В методе обязательно скобки
+print( 'Hello World' .replace('World', 'SANYYAAAAAAAAAAAAAAAAAAA VERNI SOTKU')) #Метод замены части строки
+print(''.join(['HELLO' 'gggg'   'fvsvsnjviijvf' 'fdsijvimvisv']))
+print('Hello World' .lower() )
+print('Hello World' .upper() )
+print('Hello World' .find('World') )
+
+#длина контейнера
+print(len(s)
+
+
+
+
+c = input(str('введите в  '))
+c = с.split(' ')
+print(c[::2])
+'''
+
+
+e = input(str('введите строку: '))
+f = input(str('введите строку2: '))
+print(e + '-' + f)
+
+
+
+
+
+
+
+
+
+
+
+
+
